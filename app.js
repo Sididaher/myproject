c00b6b40f315c4bd2ceeb4207ccb9ed7132cec8b
Siditaher <omarsiditaher@gmail.com>
2025-10-31 23:51:15 +0100

    changing title

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("Hello ,My name is Sid'Taher");
+//alert("Hello ,My name is Sid'Taher");
+let students = [
+      {firstname: "Sid'Taher", lastname: "Khan", age: 21},
+      {firstname: "John", lastname: "Doe", age: 22},
+      {firstname: "Jane", lastname: "Smith", age: 20}
+]
+
+const firstnames = students.map((student) => {return student.age<22});
+console.log(age);
